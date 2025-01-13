54ac6dc2be38dc768ebbd10d40864c0b938240dc
liquidmay <liquidationmayvary@gmail.com>
2025-01-13 17:06:30 +0900

    1.1

--- index.js
+++ index.js
@@ -12,7 +12,21 @@ addBtn.addEventListener("click", function () {
   }
 });
 
-function createTodo() {
+const savedTodoList = JSON.parse(localStorage.getItem("saved-items"));
+
+if (savedTodoList) {
+  for (let i = 0; i < savedTodoList.length; i++) {
+    createTodo(savedTodoList[i]);
+  }
+}
+
+function createTodo(storageData) {
+  let todoContents = todoInput.value;
+
+  if (storageData) {
+    todoContents = storageData.contents;
+  }
+
   const todoCreate = document.querySelector("#todoCreate");
   const newLi = document.createElement("li");
   const newCheck = document.createElement("input");
@@ -33,17 +47,37 @@ function createTodo() {
 
   newCheck.addEventListener("click", function () {
     todoDone(newCheck, newConn);
+
+    newLi.classList.toggle("complete");
+
+    saveItems();
   });
 
   newSpan.addEventListener("click", function () {
     newLi.style.display = "none";
   });
+
+  saveItems();
 }
 
 function todoDone(newCheck, newConn) {
   if (newCheck.checked) {
     newConn.style.textDecoration = "line-through";
+    newConn.style.color = "gray";
   } else {
     newConn.style.textDecoration = "none";
   }
+}
+
+function saveItems() {
+  const saveItem = [];
+  for (let i = 0; i < todoCreate.children.length; i++) {
+    const todoObj = {
+      contents: todoCreate.children[i].newConn.textContent,
+      complete: todoCreate.children[i].classList.contains("complete"),
+    };
+    saveItem.push(todoObj);
+  }
+  console.log(saveItem);
+  localStorage.setItem("saved-items", JSON.stringify(saveItem));
 }
