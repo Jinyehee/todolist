e8eb2db77178b91a407ad31ad21bdafaf24da6a9
unknown <ounnms@gmail.com>
2025-01-14 22:18:57 +0900

    끝

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const addBtn = document.querySelector("#addBtn");
+const todoInput = document.querySelector("#todoInput");
+const todoCreate = document.querySelector("#todoCreate");
 
 document.addEventListener("keydown", function (event) {
   if (event.key === "Enter" && todoInput.value !== "") {
@@ -12,38 +14,70 @@ addBtn.addEventListener("click", function () {
   }
 });
 
-function createTodo() {
-  const todoCreate = document.querySelector("#todoCreate");
+const savedTodoList = JSON.parse(localStorage.getItem("saved-items"));
+
+if (savedTodoList) {
+  for (let i = 0; i < savedTodoList.length; i++) {
+    createTodo(savedTodoList[i]);
+  }
+}
+
+function createTodo(storageData) {
   const newLi = document.createElement("li");
   const newCheck = document.createElement("input");
   newCheck.type = "checkbox";
   const newConn = document.createElement("span");
   const newSpan = document.createElement("span");
-  const todoInput = document.querySelector("#todoInput");
 
   newLi.appendChild(newCheck);
   newLi.appendChild(newConn);
   newLi.appendChild(newSpan);
 
-  newConn.textContent = todoInput.value;
+  let todoContents = todoInput.value;
+
+  if (storageData) {
+    todoContents = storageData.contents;
+  }
+
+  newConn.textContent = todoContents;
   newSpan.textContent = "☒";
   todoCreate.appendChild(newLi);
 
   todoInput.value = "";
 
   newCheck.addEventListener("click", function () {
-    todoDone(newCheck, newConn);
+    newLi.classList.toggle("complete");
+    saveItems();
   });
+
+  if (storageData && storageData.complete === true) {
+    newLi.classList.add('complete')
+    newCheck.checked = true;
+  }
 
   newSpan.addEventListener("click", function () {
-    newLi.style.display = "none";
+    newLi.remove();
+    saveItems();
   });
+
+  saveItems();
 }
 
-function todoDone(newCheck, newConn) {
-  if (newCheck.checked) {
-    newConn.style.textDecoration = "line-through";
-  } else {
-    newConn.style.textDecoration = "none";
+function saveItems() {
+  const saveItem = [];
+  for (let i = 0; i < todoCreate.children.length; i++) {
+    const li = todoCreate.children[i];
+    const newConn = li.querySelector("span");
+    const todoObj = {
+      contents: newConn.textContent,
+      complete: li.classList.contains("complete"),
+    };
+    saveItem.push(todoObj);
+  }
+
+  if (saveItem.length === 0) {
+    localStorage.removeItem('saved-items')
+  }else{
+      localStorage.setItem('saved-items', JSON.stringify(saveItem));
   }
 }
